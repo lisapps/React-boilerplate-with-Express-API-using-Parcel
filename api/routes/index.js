21b532ef032cbe21329e51d1576e499634e483f3
Lisa Ziegler <lisa_ziegler@kingston.com>
2020-01-28 15:00:54 -0800

    route name fixes

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 // var app = express();
 
-/* GET home page. */
 router.get("/", function(req, res) {
   //no render
   next(err);
--- client/src/components/Example.js
+++ client/src/components/Example.js
@@ -4,7 +4,7 @@ import { useAsync } from "react-async";
 
 // this calls our Express Backend on the same server during development. This must be changed to the full path in production.
 const loadApiData = async () =>
-  await fetch("http://localhost:8000/testAPI")
+  await fetch("http://localhost:8000/testRoute")
     .then(res => (res.ok ? res : Promise.reject(res)))
     .then(res => res.json());
 
@@ -15,7 +15,7 @@ function Example() {
   if (data)
     return (
       <>
-        <p className="animated fadeIn">>{data.text}</p>
+        <p className="animated fadeIn">{data.text}</p>
       </>
     );
 }
